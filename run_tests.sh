1ce055d7f26452596ea365323d2f66307d0a4bb4
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-09 09:57:48 +0530

    Added: run_tests.h

---
+++ run_tests.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+test_file=$1
+test_directory=tests_$1
+
+cp $test_file.* $test_directory 
+
+cd $test_directory
+
+rm -f *.o
+
+gcc -c *.c
+
+if [ $? == 0 ]
+then
+  gcc -o tests *.o
+
+  if [ $? == 0 ]
+  then
+    rm -f *.o
+    rm $test_file.*
+
+    ./tests
+  else
+    echo "Linking Failed."
+  fi
+
+else
+  echo "Compilation Error."
+fi
