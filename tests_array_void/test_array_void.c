a9e77d78b67919f4c37f27bd94326e11130b218d
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-05-09 19:59:20 +0530

    Added: test_array_void.c with assertion functions

---
+++ tests_array_void/test_array_void.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "test_array_void.h"
+
+unsigned NO_OF_PASSING_TEST = 0;
+unsigned NO_OF_FAILING_TEST = 0;
+
+void display_tests_status()
+{
+  printf("Passing %u, ", NO_OF_PASSING_TEST);
+  printf("Failing %u\n", NO_OF_FAILING_TEST);
+}
+
+void fail_message(Message message)
+{
+  printf("X %s\n", message);
+}
+
+void pass_message(Message message)
+{
+  printf("- %s\n", message);
+}
+
+Bool is_int_equal(Object obj_1, Object obj_2)
+{
+  return *(Int_Ptr)obj_1 == *(Int_Ptr)obj_2;
+}
+
+void assert_array_void_equal(ArrayVoid_Ptr array_1, ArrayVoid_Ptr array_2, Are_Equal is_data_equal, Message message)
+{
+  Bool are_equals = True;
+  
+  are_equals = are_equals && (array_1->length == array_2->length);
+
+  if(!are_equals)
+  {
+    NO_OF_FAILING_TEST++;
+    fail_message(message);
+    return;
+  }
+
+  for(unsigned i = 0; i < array_1->length; i++)
+  {
+    are_equals = are_equals && (*is_data_equal)(array_1->array[i], array_2->array[i]);
+  }
+
+  if(are_equals)
+  {
+    NO_OF_PASSING_TEST++;
+    pass_message(message);
+    return;
+  }
+
+  NO_OF_FAILING_TEST++;
+  fail_message(message);
+}
---
+++ tests_array_void/test_array_void.h
@@ -0,0 +1,20 @@
+#ifndef __TEST_ARRAY_VOID_H
+#define __TEST_ARRAY_VOID_H
+
+#include "array_void.h"
+
+typedef char *Message;
+typedef int *Int_Ptr;
+
+void fail_message(Message message);
+void pass_message(Message message);
+
+typedef Bool (*Are_Equal)(Object, Object);
+
+Bool is_int_equal(Object, Object);
+
+void assert_array_void_equal(ArrayVoid_Ptr, ArrayVoid_Ptr, Are_Equal, Message);
+
+void display_tests_status();
+
+#endif
